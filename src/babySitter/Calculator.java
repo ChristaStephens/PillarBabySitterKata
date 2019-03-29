499fcfccef29dc6565d21a84a6e9348e73e034d8
Christa Stephens <ChristaStephens@outlook.com>
2019-03-29 16:33:21 -0400

    changed approch - using an array list to show start/end times of Mellie the babysitter. Will be able to 'hopefully' pick from those times

--- src/babySitter/Calculator.java
+++ src/babySitter/Calculator.java
@@ -2,28 +2,27 @@ package babySitter;
 
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 
 public class Calculator {
 
-	
-
-	private Object[] startTime;
-	int[] validStartTime = {5, 6, 7, 8, 9, 10, 11};
-
 	@Test
+	//list for start and end times
      public void isStartTimeAfter5pm() {
-          MellieWageCalulator workTime = new MellieWageCalulator(startTime);
-          assertArrayEquals(5, workTime.calculateHoursWorked());
+		 List<Integer> actual = Arrays.asList(5, 6, 7);
+	        List<Integer> expected = Arrays.asList(5, 6, 7);
+	        
+	      //1. Test equal.
+	        assertThat(actual, is(expected));
 }
-
-	private void assertArrayEquals(int time, Object[] calculateHoursWorked) {
-		// TODO Auto-generated method stub
-		
-		
-	}
 
 
 }
