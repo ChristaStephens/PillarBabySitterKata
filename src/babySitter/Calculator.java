61a1ef97f4d0840843120970f91db13fd51ed74b
Christa Stephens <ChristaStephens@outlook.com>
2019-03-29 20:52:39 -0400

    created test for bed time

--- src/babySitter/Calculator.java
+++ src/babySitter/Calculator.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -17,12 +18,22 @@ public class Calculator {
 	@Test
 	//list for start and end times
      public void isStartTimeAfter5pm() {
-		 List<Integer> actual = Arrays.asList(5, 6, 7);
-	        List<Integer> expected = Arrays.asList(5, 6, 7);
+		 
+	     List<Integer> shiftStart = Arrays.asList(5, 6, 7);
 	        
-	      //1. Test equal.
-	        assertThat(actual, is(expected));
+	      //testing that time starts at five for actual
+	        assertThat(shiftStart.contains(5));
 }
+	@Test
+	public void whenIsBedTime() {
+		List<Integer> bedTime = Arrays.asList(8, 9, 10, 11, 12);
+		assertThat(bedTime.contains(8));
+	}
+
+	private void assertThat(boolean contains) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
